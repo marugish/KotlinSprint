6ffbb90af14d048cfec59f9c0c652cdd9e911e4c
marugish <marugish@yandex.ru>
2023-07-15 00:31:23 +0300

    KS-1-2 add three variables and print data

---
+++ src/main/kotlin/lesson1/lesson1-task2.kt
@@ -0,0 +1,15 @@
+package lesson1
+
+fun main() {
+
+    val numberOfOrders: UInt = 75u
+    val notificationText: String = "Thanks for shopping with us! Hope to see you again."
+    println(numberOfOrders)
+    println(notificationText)
+
+    var numberOfWorkers: UInt = 2000u
+    println(numberOfWorkers)
+    numberOfWorkers = 1999u
+    println(numberOfWorkers)
+
+}
