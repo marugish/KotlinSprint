f6e4368d0402b839214f2e6e13fb7429b1647a28
marugish <marugish@yandex.ru>
2023-07-17 00:40:42 +0300

    KS-2-4 task with buff

---
+++ src/main/kotlin/lesson2/lesson2-task4.kt
@@ -0,0 +1,10 @@
+package lesson2
+
+fun main() {
+
+    val crystalOre = 7
+    val ironOre = 11
+    println("Crystal ore buff: ${crystalOre / 5}")
+    println("Iron ore buff: ${ironOre / 5}")
+
+}
