594bb795470ac8a9fe969fceaa4fadd9a45b9ff7
marugish <marugish@yandex.ru>
2023-07-17 13:55:47 +0300

    KS-1-5 removed "the crutches"

--- src/main/kotlin/lesson1/lesson1-task5.kt
+++ src/main/kotlin/lesson1/lesson1-task5.kt
@@ -1,21 +1,12 @@
 package lesson1
 
-fun correctTimeFormat(value: Int):String {
-    var str: String = ""
-    if (value in 0 .. 9) str += "0"
-    str += value.toString()
-    return str
-}
-
 fun main() {
 
     val flightTime = 6480
     val hours = flightTime / 3600
     val minutes = (flightTime % 3600) / 60
     val seconds = flightTime % 60
-    // version 1
+
     println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
-    // version 2
-    println("${correctTimeFormat(hours)}:${correctTimeFormat(minutes)}:${correctTimeFormat(seconds)}")
 
 }
