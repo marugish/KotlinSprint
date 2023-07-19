20dc3650a36314d5028062568b51a38b7c88da4a
marugish <marugish@yandex.ru>
2023-07-19 23:49:48 +0300

    KS-5-2 hidden content

---
+++ src/main/kotlin/lesson5/lesson5-task2.kt
@@ -0,0 +1,18 @@
+package lesson5
+
+const val AGE_OF_MAJORITY = 18
+const val YEAR = 2023
+
+fun main() {
+
+    print("Введите год вашего рождения: ")
+    val userYear = readln().toInt()
+    val userAge = YEAR - userYear
+    if (userAge >= AGE_OF_MAJORITY) {
+        println("Показать экран со скрытым контентом")
+    }
+    else {
+        println("Вернуться на главный экран приложения")
+    }
+
+}
