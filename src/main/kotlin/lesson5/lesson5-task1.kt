4de7a2dd0769b45f524c28589fbba21423bdf713
marugish <marugish@yandex.ru>
2023-07-21 00:43:57 +0300

    KS-5-1 the amount is calculated automatically

--- src/main/kotlin/lesson5/lesson5-task1.kt
+++ src/main/kotlin/lesson5/lesson5-task1.kt
@@ -1,19 +1,17 @@
 package lesson5
 
-const val EXAMPLE = "2 + 2 = "
-const val ANSWER_TO_EXAMPLE = 4
+const val NUMBER_1 = 2
+const val NUMBER_2 = 3
+const val ANSWER_TO_EXAMPLE = NUMBER_1 + NUMBER_2
 
 fun main() {
 
-    print("Реши пример: $EXAMPLE")
+    print("Реши пример: $NUMBER_1 + $NUMBER_2 = ")
     val userResponse = readln().toInt()
+
     if (userResponse == ANSWER_TO_EXAMPLE)
-    {
         println("Добро пожаловать!")
-    }
     else
-    {
         println("Доступ запрещён.")
-    }
 
 }
